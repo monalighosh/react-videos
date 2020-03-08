70b407ce19aa92a59aaead84cce01d350a6ac391
Monali Ghosh <monalighosh@users.noreply.github.com>
2020-03-08 23:19:35 +1100

    Updates

---
+++ precache-manifest.dd5a6dc6c227530b4f6cdc914d7d7d60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e5f9d2dd2bf6170763711f6c8d2bcae",
+    "url": "/react-videos/index.html"
+  },
+  {
+    "revision": "f41589cf4afe8643bfa6",
+    "url": "/react-videos/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0b6bc1552673249318f5",
+    "url": "/react-videos/static/js/2.f1f0ab91.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-videos/static/js/2.f1f0ab91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f41589cf4afe8643bfa6",
+    "url": "/react-videos/static/js/main.1cdb8d65.chunk.js"
+  },
+  {
+    "revision": "72b0f6a6ee81c8c1c9c2",
+    "url": "/react-videos/static/js/runtime-main.d6485180.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-videos/static/media/logo.5d5d9eef.svg"
+  }
+]);
