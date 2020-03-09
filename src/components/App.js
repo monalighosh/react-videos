7d641d6583059d45b39a6a2e3ee4b2fcd50d7c1a
Monali Ghosh <monalighosh@users.noreply.github.com>
2020-03-09 11:46:38 +1100

    Updating state updates and video details default search

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,10 @@ class App extends React.Component {
     selectedVideo: null
   };
 
+  componentDidMount() {
+    this.onSearchSubmit('cars');
+  }
+
   onSearchSubmit = async (searchValue) => {
     const response = await youtube.get('/search', {
       params: {
@@ -19,7 +23,10 @@ class App extends React.Component {
       }
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = (video) => {
@@ -27,18 +34,25 @@ class App extends React.Component {
   };
   
   render() {
-   
     return (
       <div className='ui container'>
         <SearchBar
           placeholder='Search videos here...'
           onSearchSubmit={this.onSearchSubmit}
         />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={this.onVideoSelect}
-        />
+        <div className='ui grid'>
+          <div className='ui row'>
+            <div className='eleven wide column'>
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className='five wide column'>
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
